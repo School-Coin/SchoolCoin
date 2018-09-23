fcddb45f8327a2b3e52f9511ccfb6c1c3f00514b
SchoolCoin <42725019+school-coin@users.noreply.github.com>
2018-09-23 13:41:38 -0700

    Update WalletConfig.h

--- src/config/WalletConfig.h
+++ src/config/WalletConfig.h
@@ -23,7 +23,7 @@ namespace WalletConfig
     const std::string addressBookFilename = ".addressBook.json";
 
     /* The name of your deamon */
-    const std::string daemonName = "SchoolCoin";
+    const std::string daemonName = "SchoolCoind";
 
     /* The name to call this wallet */
     const std::string walletName = "SCCwallet";
